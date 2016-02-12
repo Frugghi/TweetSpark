3ab55c3c1cda0bc4203608447cd2ba1d07a97d58
Tommaso Madonia <tommaso@madonia.me>
2016-02-13 00:37:28 +0100

    Profiler should be configured manually

---
+++ vagrant-cluster/resources/profiler/update-profiler-cluster.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+source "$VAGRANT_RES_DIR/common.sh"
+
+SERVER="monitor.cluster"
+PORT="8086"
+REPORTER="InfluxDBReporter"
+DATABASE="profiler"
+USERNAME="profiler"
+PASSWORD="profiler"
+WHITELIST=""
+BLACKLIST=""
+
+while getopts s:r:d:u:p:w:b: option
+do
+    case "${option}"
+    in
+        s) SERVER=${OPTARG};;
+        r) REPORTER=${OPTARG};;
+        d) DATABASE=${OPTARG};;
+        u) USERNAME=${OPTARG};;
+        p) PASSWORD=${OPTARG};;
+        w) WHITELIST=${OPTARG};;
+        b) BLACKLIST=${OPTARG};;
+    esac
+done
+
+echo "Updating profiler settings..."
+local profiler="$VAGRANT_RES_DIR/profiler/update-profiler-node.sh"
+local profiler_jar=""
+if [ -e "$VAGRANT_RES_DIR/profiler/profiler" ]; then
+	profiler_jar=`cat "$VAGRANT_RES_DIR/profiler/profiler"`
+else
+	profiler_jar=`basename $(find "$VAGRANT_RES_DIR/profiler" -name 'statsd-jvm-profiler*.jar')`
+fi
+
+local profiler_options="-javaagent:/home/vagrant/resources/profiler/${profiler_jar}=server=${SEVER},port=${PORT},reporter=${REPORTER},database=${DATABASE},username=${USERNAME},password=${PASSWORD},prefix=bigdata.profiler.$(basename $1 .jar).USERNAME_HERE.${2//\//}.$RANDOM.$(date +'%y%m%d-%H%M'),tagMapping=SKIP.SKIP.username.job.flow.stage.phase"
+if [ ! -z "$WHITELIST" ]; then
+    profiler_options="${profiler_options},packageWhitelist=${WHITELIST}"
+fi
+if [ ! -z "$BLACKLIST" ]; then
+    profiler_options="${profiler_options},packageBlacklist=${BLACKLIST}"
+fi
+
+$profiler "$profiler_options"
+while IFS= read -r host
+do
+	ssh -n $host "$profiler $profiler_options"
+done < <( grep -o -e 'slave[0-9]*\.cluster$' /etc/hosts )
+${HADOOP_PREFIX}/sbin/stop-yarn.sh
+${HADOOP_PREFIX}/sbin/start-yarn.sh
---
+++ vagrant-cluster/resources/profiler/update-profiler-node.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+source "$VAGRANT_RES_DIR/common.sh"
+
+COMMENT=""
+OPTIONS="$1"
+if [ -z "$OPTIONS" ]; then
+	COMMENT="#"
+fi
+
+#sed -i -r "s|#?export _JAVA_OPTIONS=.*|${COMMENT}export _JAVA_OPTIONS=\"${OPTIONS//USERNAME_HERE/hadoop}\"|" $HADOOP_CONF/hadoop-env.sh
+#sed -i -r "s|#?export _JAVA_OPTIONS=.*|${COMMENT}export _JAVA_OPTIONS=\"${OPTIONS//USERNAME_HERE/mapred}\"|" $HADOOP_CONF/mapred-env.sh
+sed -i -r "s|#?export _JAVA_OPTIONS=.*|${COMMENT}export _JAVA_OPTIONS=\"${OPTIONS//USERNAME_HERE/yarn}\"|" $HADOOP_CONF/yarn-env.sh
+#sed -i -r "s|#?spark.driver.extraJavaOptions .*|${COMMENT}spark.driver.extraJavaOptions ${OPTIONS//USERNAME_HERE/spark-driver}|" $SPARK_CONF/spark-defaults.conf
+#sed -i -r "s|#?spark.executor.extraJavaOptions .*|${COMMENT}spark.executor.extraJavaOptions ${OPTIONS//USERNAME_HERE/spark-executor}|" $SPARK_CONF/spark-defaults.conf
